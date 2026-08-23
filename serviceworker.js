bc0bea6b7798ae03d4c13b1908c89e37c6311628
neelisettypragna-star <neelisettypragna@gmail.com>
2026-08-23 09:09:16 +0530

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = "safetour-v2";
+const urlsToCache = ["/", "/index.html"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
